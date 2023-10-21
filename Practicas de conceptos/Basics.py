cf8363e5ff356d4db892a057dd3935e82cb78ff1
Johan González <soyelsxbbsx@outlook.com>
2023-10-21 18:36:04 -0500

    Inicio

---
+++ Practicas de conceptos/Basics.py
@@ -0,0 +1,5 @@
+cadena = "Hola mundo"
+print(f"Hola, {str.upper(cadena)}")
+print(cadena)
+cadena = cadena.upper()
+print(cadena)
--- Practicas de conceptos/Interprete.py
+++ Practicas de conceptos/Interprete.py
@@ -1,1 +1,14 @@
-import fibo
+import fibo as a
+# fibo.fib(500)
+
+# fib = fibo.fib
+# a = 1000
+
+# fib(a)
+
+a.fib(500)
+
+
+from fibo import fib as b
+
+b(500)
